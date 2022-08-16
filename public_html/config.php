fcf0b761305defcb53afc516a278c6736ee7f91c
KASARIKS <golovko085@gmail.com>
2022-08-16 12:41:12 +0700

    3-rd lesson

---
+++ public_html/config.php
@@ -0,0 +1,10 @@
+<?php
+defined ('VG_ACCESS') or die ('Access denied');
+
+const SITE_URL = 'http://im.my';
+const PATH = '/';
+
+const HOST = '127.0.0.1';
+const USER = 'root';
+const PASS = 'root';
+const DB_NAME = 'im';
---
+++ public_html/core/base/settings/internal_settings.php
@@ -0,0 +1,19 @@
+<?php
+
+defined ('VG_ACCESS') or die ('Access denied');
+
+const TEMPLATE = 'templates/default/';
+const ADMIN_TEMPLATES = 'core/admin/views/';
+
+const COKKIE_VERSION = '1.0.0';
+const CRYPT_KEY = '';
+const COKKIE_TIME = 60;
+const BLOCK_TIME = 3;
+
+const QTY = 8;
+const QTY_LINKS = 3;
+
+const USER_CSS_JS = [
+    'styles' => '',
+    'scripts' => ''
+];
